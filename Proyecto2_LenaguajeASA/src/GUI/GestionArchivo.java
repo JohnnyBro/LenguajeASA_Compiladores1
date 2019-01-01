7be9f51f06d0e3215f516867c2f172f21c1e7d59
JohnnyBro <jbravozam@gmail.com>
2019-01-01 02:53:29 -0600

    Interfaz Grafica

---
+++ Proyecto2_LenaguajeASA/src/GUI/GestionArchivo.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+import java.io.*;
+/**
+ *
+ * @author johnnybravo
+ */
+public class GestionArchivo {
+    
+    FileInputStream entrada;
+    FileOutputStream salida;
+    File archivo;
+    public GestionArchivo()
+    {
+        
+    }
+    
+    public String AbrirATexto(File archivo){
+        String contenido="";
+        try {
+            entrada = new FileInputStream(archivo);
+            int ascci;
+            while((ascci = entrada.read())!= -1){
+                char carcater = (char)ascci;
+                contenido += carcater;
+            }
+        } catch (Exception e) {
+        }
+        return contenido;
+    }
+    
+    public String GuardarATexto(File archivo, String contenido)
+        {
+        String respuesta=null;
+        try {
+            salida = new FileOutputStream(archivo);
+            byte[] bytesTxt = contenido.getBytes();
+            salida.write(bytesTxt);
+            respuesta = "Se guardo con exito el archivo";
+        } catch (Exception e) {
+        }
+        return respuesta;
+    }
+        
+        
+          
+    }
+
--- Proyecto2_LenaguajeASA/src/GUI/Proyecto2_LenaguajeASA.java
+++ Proyecto2_LenaguajeASA/src/GUI/Proyecto2_LenaguajeASA.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto2_lenaguajeasa;
+package GUI;
 
 /**
  *
@@ -16,6 +16,8 @@ public class Proyecto2_LenaguajeASA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        PanelPrincipal panel=new PanelPrincipal();
+        panel.setVisible(true);
     }
     
 }
